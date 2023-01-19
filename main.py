5167515e0f0d2e7f090f3ecbd17abfef86e2e389
Ilia Nikolaenko <niil02@sms-group.com>
2023-01-19 16:01:39 +0100

    Main: Added update test transactions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 
@@ -5,23 +7,40 @@ from db_engines.sql_server_engine import engine_SQLServerTest_MainDB
 
 select1_stmt = text("SELECT id, category, name FROM Cracs_preventer_test.signal_meta ORDER BY id, category")
 select2_stmt = text("SELECT id, category, name FROM Cracs_preventer_test.signal_meta WHERE id > :id ORDER BY id, category")
+select3_stmt = text("SELECT id, name, description, update_date FROM Cracs_preventer_test.signal_meta WHERE id = :id ORDER BY id")
+update1_stmt = text("UPDATE Cracs_preventer_test.signal_meta SET update_date=:upd_date, description=:descr WHERE id=:id")
 
-# It’s recommended that the object "Connection" is used in context manager style using the Python "with:" statement. It
-# represents active database resources and it’s good to make sure it’s closed when operations are completed
-with engine_SQLServerTest_MainDB.connect() as connection:
-    result = connection.execute(select1_stmt)
-    counter = 1
-    for row in result:
-        print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
-        counter += 1
+# # It’s recommended that the object "Connection" is used in context manager style using the Python "with:" statement. It
+# # represents active database resources and it’s good to make sure it’s closed when operations are completed
+# with engine_SQLServerTest_MainDB.connect() as connection:
+#     result = connection.execute(select1_stmt)
+#     counter = 1
+#     for row in result:
+#         print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
+#         counter += 1
+#
+# # It’s recommended that the fundamental transactional / database interactive ORM object "Session" is used in
+# # context manager style using the Python "with:" statement. It represents active database resources, and it’s good to
+# # make sure it’s closed when operations are completed
+# with Session(engine_SQLServerTest_MainDB) as session:
+#     result = session.execute(select1_stmt)
+#     for row in result:
+#         print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
+#         counter += 1
 
-# It’s recommended that the fundamental transactional / database interactive ORM object "Session" is used in
-# context manager style using the Python "with:" statement. It represents active database resources, and it’s good to
-# make sure it’s closed when operations are completed
 with Session(engine_SQLServerTest_MainDB) as session:
-    result = session.execute(select1_stmt)
-    for row in result:
-        print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
-        counter += 1
+    result1 = session.execute(update1_stmt, {"upd_date": "2023-19-01 00:00:00.000", "descr": "This is a test object", "id": "test_obj_1"})
+    session.commit()
+
+    result2 = session.execute(select3_stmt, {"id": "test_obj_1"})
+    for row in result2:
+        print(row["id"], row["name"], "last updated on:", row["update_date"], "with description:", row["description"])
+
+    result3 = session.execute(update1_stmt, {"upd_date": datetime.utcnow(), "descr": "New test description 1", "id": "test_obj_1"})
+    session.commit()
+
+    result4 = session.execute(select3_stmt, {"id": "test_obj_1"})
+    for row in result4:
+        print(row["id"], row["name"], "last updated on:", row["update_date"], "with description:", row["description"])
 
 
