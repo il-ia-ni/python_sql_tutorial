81db49c48eeac5588098160612e208762c2da074
Ilia Nikolaenko <niil02@sms-group.com>
2023-01-19 13:39:50 +0100

    Upd main script

--- ddl_scripts/adding_data.py
+++ ddl_scripts/adding_data.py
@@ -4,9 +4,11 @@ from db_engines.sql_server_engine import engine_SQLServerTest_MainDB
 from ddl_scripts.creating_tables import SignalMeta
 
 # See https://docs.sqlalchemy.org/en/14/tutorial/dbapi_transactions.html#tutorial-executing-orm-session
+# https://docs.sqlalchemy.org/en/14/orm/session_basics.html#id1
 with Session(engine_SQLServerTest_MainDB) as session:
-    # It’s recommended that the Session is used in context manager style using the Python "with:" statement. The Session
-    # object represents active database resources and it’s good to make sure it’s closed when operations are completed
+    # It’s recommended that the fundamental transactional / database interactive ORM object "Session" is used in
+    # context manager style using the Python "with:" statement. The Session object represents active database resources,
+    # and it’s good to make sure it’s closed when operations are completed
     testDataObj1 = SignalMeta(
         id="test_obj_1",
         create_date="2023-19-01 00:00:00.000",  # TODO: Try out with Python datetimes?
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
 
 from db_engines.sql_server_engine import engine_SQLServerTest_MainDB
+
+select1_stmt = text("SELECT id, category, name FROM Cracs_preventer_test.signal_meta ORDER BY id, category")
+select2_stmt = text("SELECT id, category, name FROM Cracs_preventer_test.signal_meta WHERE id > :id ORDER BY id, category")
 
 # It’s recommended that the connect() is used in context manager style using the Python "with:" statement. The Engine
 # object represents active database resources and it’s good to make sure it’s closed when operations are completed
 with engine_SQLServerTest_MainDB.connect() as connection:
-    result = connection.execute(text("select id, category, name from Cracs_preventer_test.signal_meta"))
+    result = connection.execute(select1_stmt)
     counter = 1
+    for row in result:
+        print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
+        counter += 1
+
+# It’s recommended that the fundamental transactional / database interactive ORM object "Session" is used in
+# context manager style using the Python "with:" statement. The Session object represents active database resources,
+# and it’s good to make sure it’s closed when operations are completed
+with Session(engine_SQLServerTest_MainDB) as session:
+    result = session.execute(select1_stmt)
     for row in result:
         print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
         counter += 1
