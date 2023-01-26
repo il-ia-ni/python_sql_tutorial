fc4502f79350f580ebd5b13926f39480f4956574
Ilia Nikolaenko <niil02@sms-group.com>
2023-01-26 12:09:19 +0100

    Refactor simple_selects to Core and ORM APIs

--- dql_scripts/simple_select.py
+++ dql_scripts/simple_select.py
@@ -6,36 +6,49 @@ from sqlalchemy.orm import Session
 from db_engines.sql_server_engine import engine_SQLServerTest_MainDB
 from ddl_scripts.creating_tables import SignalMeta
 
-# Selecting Rows with Core API or ORM API: https://docs.sqlalchemy.org/en/14/tutorial/data_select.html#tutorial-selecting-data
-# See more to SQLAlchemy Core, ORM APIs @ https://docs.sqlalchemy.org/en/14/tutorial/index.html
-
 select1_stmt = text("SELECT id, category, name FROM Cracs_preventer_test.signal_meta ORDER BY id, category")
 select2_stmt = text("SELECT id, category, name FROM Cracs_preventer_test.signal_meta WHERE id > :id ORDER BY id, category")
 select3_stmt = text("SELECT id, name, description, update_date FROM Cracs_preventer_test.signal_meta WHERE id = :id ORDER BY id")
 update1_stmt = text("UPDATE Cracs_preventer_test.signal_meta SET update_date=:upd_date, description=:descr WHERE id=:id")
 
-# # It’s recommended that the object "Connection" is used in context manager style using the Python "with:" statement. It
-# # represents active database resources and it’s good to make sure it’s closed when operations are completed
-# with engine_SQLServerTest_MainDB.connect() as connection:
-#     result = connection.execute(select1_stmt)
-#     counter = 1
-#     for row in result:
-#         print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
-#         counter += 1
-#
-# # It’s recommended that the fundamental transactional / database interactive ORM object "Session" is used in
-# # context manager style using the Python "with:" statement. It represents active database resources, and it’s good to
-# # make sure it’s closed when operations are completed
-# # See https://docs.sqlalchemy.org/en/14/tutorial/dbapi_transactions.html#tutorial-executing-orm-session
-# with Session(engine_SQLServerTest_MainDB) as session:
-#     result = session.execute(select1_stmt)  # Session.execute() is used the same way as Connection.execute()
-#     for row in result:
-#         print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
-#         counter += 1
+""" Connection.execute() in Core API
+Selecting Rows with Core API or ORM API: https://docs.sqlalchemy.org/en/14/tutorial/data_select.html#tutorial-selecting-data
+See more to SQLAlchemy Core, ORM APIs @ https://docs.sqlalchemy.org/en/14/tutorial/index.html 
+"""
+
+
+def select_core_signalmeta_all():
+    # It’s recommended that the object "Connection" is used in context manager style using the Python "with:" statement.
+    # It represents active database resources and it’s good to make sure it’s closed when operations are completed
+    with engine_SQLServerTest_MainDB.connect() as connection:
+        result = connection.execute(select1_stmt)
+        counter = 1
+        for row in result:
+            print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
+            counter += 1
+
+
+""" Session.execute() in ORM API 
+Selecting Rows with Core API or ORM API: https://docs.sqlalchemy.org/en/14/tutorial/data_select.html#tutorial-selecting-data
+See more to SQLAlchemy Core, ORM APIs @ https://docs.sqlalchemy.org/en/14/tutorial/index.html 
+"""
+
+
+def select_orm_signalmeta_all():
+    # It’s recommended that the fundamental transactional / database interactive ORM object "Session" is used in
+    # context manager style using the Python "with:" statement. It represents active database resources, and it’s good to
+    # make sure it’s closed when operations are completed
+    # See https://docs.sqlalchemy.org/en/14/tutorial/dbapi_transactions.html#tutorial-executing-orm-session
+    counter = 1
+    with Session(engine_SQLServerTest_MainDB) as session:
+        result = session.execute(select1_stmt)  # Session.execute() is used the same way as Connection.execute()
+        for row in result:
+            print(f"id{counter}:", row["id"], f" has a category", row["category"], "with name:", row["name"])
+            counter += 1
 
 
 # noinspection SpellCheckingInspection
-def select_signalmeta_testobjs():
+def select_orm_signalmeta_testobjs():
     # See https://docs.sqlalchemy.org/en/14/tutorial/dbapi_transactions.html#tutorial-executing-orm-session
     with Session(engine_SQLServerTest_MainDB) as session:
         """ "Release" ans connection pools
@@ -70,7 +83,7 @@ def select_signalmeta_testobjs():
 
 
 # noinspection SpellCheckingInspection
-def select_signalmeta_testobjs_scalar_result():
+def select_orm_signalmeta_testobjs_scalar_result():
     """
     The method that is often useful when querying for ORM objects is the Session.scalars() method, which will return
     a ScalarResult filtering object (A wrapper for a Result that returns scalar values rather than Row values) which
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import dql_scripts.simple_select as simple_select
 
-simple_select.select_signalmeta_testobjs()  # Returns a _engine.Result obj with Row objs
+simple_select.select_core_signalmeta_all()  # Selection from the table using Core API
+simple_select.select_orm_signalmeta_all()  # Selection from the table using ORM API
 
-simple_select.select_signalmeta_testobjs_scalar_result()  # Returns selection result as a Scalar obj
+simple_select.select_orm_signalmeta_testobjs()  # Returns a _engine.Result obj with Row objs
+simple_select.select_orm_signalmeta_testobjs_scalar_result()  # Returns selection result as a Scalar obj
 
