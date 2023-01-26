3ab3ff1547672176e5572808161ee141fbe6da3a
Phillip-Oliver <54893338+Phillip-Oliver@users.noreply.github.com>
2023-01-26 16:36:44 +0100

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -2,19 +2,13 @@
 Defines SQLAlchemy database connectors.
 """
 from typing import Optional
-
-import pyodbc
 from loguru import logger
 from sqlalchemy.engine import URL, Engine, create_engine, make_url
 from sqlalchemy.orm import Session, sessionmaker
 
-from cracs_backend import settings
-
 
 def build_full_url(url: str) -> URL:
-    """Generates a new database connection string based on the application settings.
-
-    Adds "odbc_driver" param to the URL when pyodbc connector is used.
+    """Generates a new database connection string.
 
     Args:
         url: database connection string
@@ -30,10 +24,6 @@ def make_engine(db_url: URL, timeout: int) -> Engine:
         db_url: connection string to use
         timeout: connection timeout in seconds
     """
-    # It's recommended to disable internal pyodbc pooling. See:
-    # https://docs.sqlalchemy.org/en/14/dialects/mssql.html#pyodbc-pooling-connection-close-behavior
-    if db_url.drivername == "mssql+pyodbc":
-        pyodbc.pooling = False
 
     return create_engine(
         db_url,
@@ -41,20 +31,8 @@ def make_engine(db_url: URL, timeout: int) -> Engine:
     )
 
 
-app_db_url = build_full_url(settings.app_db_url, settings.app_odbc_driver)
-app_engine = make_engine(app_db_url, settings.app_connection_timeout)
-
-pdw_url = build_full_url(settings.pdw_db_url, settings.pdw_odbc_driver)
-pdw_engine = make_engine(pdw_url, settings.pdw_connection_timeout)
-
-
-def get_app_db() -> Session:
-    """Returns a new Session on App DB"""
-    session = sessionmaker(app_engine, expire_on_commit=False)
-    return session()
-
-
-def get_pdw_db() -> Session:
-    """Returns a new Session on PDW DB"""
-    session = sessionmaker(pdw_engine, expire_on_commit=False)
+def get_session(engine: Engine) -> Session:
+    """Returns a new Session
+    """
+    session = sessionmaker(engine, expire_on_commit=False)
     return session()
