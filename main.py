b2d8fad9d4a416de1713ba8e7811ade089ec37ba
Ilia Nikolaenko <niil02@sms-group.com>
2023-01-26 11:59:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from datetime import datetime
-
-from sqlalchemy import create_engine, text, select
-from sqlalchemy.orm import Session
-
-from db_engines.sql_server_engine import engine_SQLServerTest_MainDB
-from ddl_scripts.creating_tables import SignalMeta
 import dql_scripts.simple_select as simple_select
 
 simple_select.select_signalmeta_testobjs()  # Returns a _engine.Result obj with Row objs
