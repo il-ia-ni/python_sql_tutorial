efa467909a2ff63203e53856d4f2b77995431730
Ilia Nikolaenko <niil02@sms-group.com>
2023-01-30 11:57:17 +0100

    Update debug_formatter.py

--- loguru_logging/debug_formatter.py
+++ loguru_logging/debug_formatter.py
@@ -5,7 +5,7 @@ def debug_format():
     # Custom debug level formatter, saves debug mssgs to a log file
     logger.add(
         # See: https://loguru.readthedocs.io/en/stable/api/logger.html#file
-        "_logs/debug_{time:YY-MM-DD-HH-mm-ss!UTC+1}.log",  # A sink in form of a path to a file: https://loguru.readthedocs.io/en/stable/api/logger.html#sink
+        "_logs/debug_{time:YY-MM-DD-HH-mm-ss}.log",  # A sink in form of a path to a file: https://loguru.readthedocs.io/en/stable/api/logger.html#sink
         level="DEBUG",
         format="Custom log: {level} | {time:HH:mm:ss!UTC} | {module}.{function} | {message}",
         rotation="1 KB",
