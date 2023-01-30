bd3ca7272e416e2619b427d47b0402270690811d
Ilia Nikolaenko <niil02@sms-group.com>
2023-01-30 11:26:12 +0100

    upd loguru rotation for custom debug logs

--- loguru_logging/debug_formatter.py
+++ loguru_logging/debug_formatter.py
@@ -4,10 +4,11 @@ from loguru import logger
 def debug_format():
     # Custom debug level formatter, saves debug mssgs to a log file
     logger.add(
+        # See: https://loguru.readthedocs.io/en/stable/api/logger.html#file
         "_logs/debug_{time:YY-MM-DD-HH-mm-ss!UTC}.log",
         level="DEBUG",
         format="Custom log: {level} | {time:HH:mm:ss!UTC} | {module}.{function} | {message}",
-        rotation="10 KB",
+        rotation="1 KB",
         retention="10 seconds",
         colorize=True
     )
