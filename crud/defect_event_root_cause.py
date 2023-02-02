2c7889b19c5057467fad0a226147b0c037679fe5
Phillip-Oliver <54893338+Phillip-Oliver@users.noreply.github.com>
2023-02-02 12:18:12 +0100

    Update defect_event_root_cause.py

--- crud/defect_event_root_cause.py
+++ crud/defect_event_root_cause.py
@@ -1,8 +1,11 @@
+import datetime
+from typing import List
 import pandas as pd
 from enum import Enum
 from db_models.defect_root_cause import DefectRootCause
 from db_models.defect_event import DefectEvent
-from sqlalchemy import select
+from sqlalchemy import select, cast
+from sqlalchemy.types import DateTime, String
 from db_connection import create_session_from_url
 
 
@@ -51,8 +54,12 @@ def join_defect_event_root_cause_filter_event_id(event_id_to_filter: int) -> pd.
     return data
 
 
-def join_defect_event_root_cause_filter_behaviour_id(defect: BehaviourPattern) -> pd.DataFrame:
-    join_query = select(DefectEvent.event_type,
+def join_defect_event_root_cause_filter_behaviour_id(defects: List[BehaviourPattern],
+                                                     strand_ids: List[String],
+                                                     start: DateTime(),
+                                                     end: DateTime()) -> pd.DataFrame:
+    join_query = select(DefectEvent.create_date,
+                        DefectEvent.event_type,
                         DefectEvent.event_id,
                         DefectEvent.caster_id,
                         DefectEvent.strand_id,
@@ -62,8 +69,10 @@ def join_defect_event_root_cause_filter_behaviour_id(defect: BehaviourPattern) -
                         DefectRootCause.signal_id,
                         DefectRootCause.importance
                         ).join(DefectEvent,
-                               DefectRootCause.event_id == DefectEvent.event_id).filter(
-        DefectEvent.behaviour_pattern_id == defect)
+                               DefectRootCause.event_id == DefectEvent.event_id)\
+        .filter(DefectEvent.behaviour_pattern_id.in_(defects))\
+        .filter(DefectEvent.strand_id.in_(strand_ids))\
+        .filter(DefectEvent.create_date.between(start, end))
     result = session.execute(join_query).fetchall()
     column_names = join_query.columns.keys()
     data = pd.DataFrame(data=result, columns=column_names)
@@ -76,5 +85,8 @@ if __name__ == "__main__":
     session = create_session_from_url(url=URL, odbc_driver=app_odbc_driver)
     data = join_defect_event_root_cause()
     data_1 = join_defect_event_root_cause_filter_event_id(event_id_to_filter=19826)
-    data_2 = join_defect_event_root_cause_filter_behaviour_id(BehaviourPattern.BULGING)
+    data_2 = join_defect_event_root_cause_filter_behaviour_id(defects=[BehaviourPattern.BULGING],
+                                                              strand_ids=['1_1'],
+                                                              start='2022-12-20 14:58:36',
+                                                              end='2022-12-20 15:58:36')
     print('')
