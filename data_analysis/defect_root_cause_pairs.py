27efbdff02987ed9ee817b62ed43fe94cc6c3784
Ilia Nikolaenko <niil02@sms-group.com>
2023-02-24 15:15:22 +0100

    Added caster filter for extract_root_cause_pairs + TODO
    Single value-tuple does not with f-Strings SQL Queries that contain WITH IN!

--- data_analysis/defect_root_cause_pairs.py
+++ data_analysis/defect_root_cause_pairs.py
@@ -1,4 +1,8 @@
 """ This script contains methods to analyse the defect root cause based on prepared signal patterns """
+from __future__ import annotations
+
+from enum import Enum
+
 from sqlalchemy import text, bindparam
 from sqlalchemy.orm import Session
 import pandas as pd
@@ -8,6 +12,13 @@ from db_engines.db_sources_data.sql_server_test_localhost import SQLServerTestDB
 from db_engines.sql_server_engine import url_SQLServerTestDBMS
 from phillip.db_connection import create_session_from_url
 
+
+class Casters(str, Enum):
+    """Defines allowed casters' names"""
+    CASTER_1 = "1"
+    CASTER_2 = "2c"
+
+
 # non-scalar variables (Sets, Tuples, Lists can be added to text queries can be added using expanding bind parameters:
 # https://stackoverflow.com/a/56382828
 # https://docs.sqlalchemy.org/en/20/core/sqlelement.html#sqlalchemy.sql.expression.text
@@ -16,7 +27,7 @@ create_root_cause_groups_text_clause = text(
     "   SELECT A.create_date, A.event_id, B.signal_id, A.behaviour_pattern_id, A.strand_id, A.slab_id, B.importance "
     "   FROM main.defect_event A INNER JOIN main.defect_root_cause B "
     "       on A.event_id=B.event_id "
-    "   WHERE A.event_id IN ("
+    "   WHERE A.caster_id IN :casters AND A.event_id IN ("
     "       SELECT event_id "
     "       FROM main.defect_root_cause "
     "       WHERE signal_id NOT in :signals "
@@ -40,7 +51,7 @@ create_root_cause_groups_text_clause = text(
     "GROUP BY p.event_id, create_date, behaviour_pattern_id, strand_id, slab_id, importance")
 
 
-def root_cause_pairs_query(date: str, signals: tuple[str, ...]) -> str:
+def root_cause_pairs_query(date: str, casters: tuple[str, ...] | str, signals: tuple[str, ...] | str) -> str:
     # tuple[str] vs tuple[str, ...] see: https://stackoverflow.com/questions/72001132/python-typing-tuplestr-vs-tuplestr
 
     # f"" is formatted string literal. An overview of Python String formatting: https://realpython.com/python-f-strings/
@@ -50,7 +61,7 @@ def root_cause_pairs_query(date: str, signals: tuple[str, ...]) -> str:
         SELECT A.create_date, A.event_id, B.signal_id, A.behaviour_pattern_id, A.strand_id, A.slab_id, B.importance
         FROM main.defect_event A INNER JOIN main.defect_root_cause B 
             on A.event_id=B.event_id 
-        WHERE A.event_id IN (
+        WHERE A.caster_id in {casters} AND A.event_id IN (
             SELECT event_id 
             FROM main.defect_root_cause
             WHERE signal_id NOT IN {signals}
@@ -73,18 +84,32 @@ def root_cause_pairs_query(date: str, signals: tuple[str, ...]) -> str:
 def extract_root_cause_pairs(
         session: Session,
         date: str,
-        signals_to_filter: tuple[str, ...]
+        casters_to_filter: tuple[Casters, ...] | Casters,
+        signals_to_filter: tuple[str, ...] | str
 ) -> pd.DataFrame:
     # tuple[str] vs tuple[str, ...] see: https://stackoverflow.com/questions/72001132/python-typing-tuplestr-vs-tuplestr
 
     # Option 1:
     # Solution for binding non-scalar vars as param of string queries: https://stackoverflow.com/a/56382828
-    # text_statement = create_root_cause_groups_text_clause.bindparams(bindparam('signals', expanding=True))
+    # text_statement = create_root_cause_groups_text_clause.bindparams(
+    #     bindparam('casters', expanding=True),
+    #     bindparam('signals', expanding=True)
+    # )
 
     # Option 2:
-    query = root_cause_pairs_query(date=date, signals=signals_to_filter)
+    if isinstance(casters_to_filter, tuple):
+        query = root_cause_pairs_query(
+            date=date, signals=signals_to_filter,
+            casters=tuple(map(lambda x: x.value, casters_to_filter))  # https://realpython.com/python-lambda/#classic-functional-constructs
+        )
+    elif isinstance(casters_to_filter, str):  # TODO: for f-String statements there has to be another statement with no "with in"!
+        query = root_cause_pairs_query(date=date, signals=signals_to_filter, casters=tuple(casters_to_filter))
     with session as s:
-        # result = s.execute(text_statement, params={'signals': signals_to_filter, 'start_date': date})
+        # result = s.execute(text_statement, params={
+        #     'start_date': date,
+        #     'casters': casters_to_filter,
+        #     'signals': signals_to_filter
+        # })
         result = s.execute(query)
 
         column_names = list(result.keys())
@@ -100,6 +125,8 @@ if __name__ == "__main__":
     data = extract_root_cause_pairs(
         session=session,
         date='2022-12-23',
-        signals_to_filter=("casting_length_1", "casting_length_C")
+        casters_to_filter=(Casters.CASTER_1, Casters.CASTER_1),  # TODO: for f-String statements there needs to be another implementation for single values
+        signals_to_filter=("casting_length_1", "casting_length_C")  # TODO: for f-String statements there needs to be another implementation for single values
     )
+    logger.info(data.info())
     logger.info(data)
