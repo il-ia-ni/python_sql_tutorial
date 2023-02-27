7315d2bb826d9bceacc1c21179d176b262140818
Ilia Nikolaenko <niil02@sms-group.com>
2023-02-27 12:27:07 +0100

    upd SQLite connection data to create engines

--- db_engines/db_sources_data/sqlite_test.py
+++ db_engines/db_sources_data/sqlite_test.py
@@ -2,11 +2,11 @@
 # https://docs.sqlalchemy.org/en/14/dialects/index.html
 # https://docs.sqlalchemy.org/en/14/dialects/sqlite.html
 
-# TODO: always check, if the sqlite-file is available at "../sqlite_db/"
+# TODO: always check, if the sqlite-file is available at "../local_repos/"
 from config.definitions import ROOT_DIR  # Found @ https://towardsdatascience.com/simple-trick-to-work-with-relative-paths-in-python-c072cdc9acb9
 
 dbdriver = "sqlite+pysqlite"  # https://docs.sqlalchemy.org/en/14/core/engines.html#sqlite
 # pysqlite dialect: https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#pysqlite
 # It uses Python module sqlite3 (by default -> "pysqlite" is opt): https://docs.python.org/3/library/sqlite3.html
-dbpath = f"//{ROOT_DIR}\\db_engines\\sqlite_db\\testdb.sqlite"
+dbpath = f"//{ROOT_DIR}\\local_repos\\fallback_db"
 
--- db_engines/sqlite_engine.py
+++ db_engines/sqlite_engine.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine
 from loguru import logger
 
 from db_engines.db_sources_data.sqlite_test import dbdriver, dbpath
+from phillip.db_connection import build_full_url, make_engine
 
 # For establishing connections see https://docs.sqlalchemy.org/en/14/tutorial/engine.html#tutorial-engine
 # For configuring the engine see https://docs.sqlalchemy.org/en/14/core/engines.html#sqlite
 # For working with engines see https://docs.sqlalchemy.org/en/14/core/connections.html
 
-url_SQLite_TestDB = f"{dbdriver}:/{dbpath}"  # TODO: always check, if the sqlite-file is available at "./sqlite_db/"
+url_SQLite_TestDB = f"{dbdriver}:/{dbpath}"  # TODO: always check, if the sqlite-file is available at "../local_repos/"
 test_mssg = url_SQLite_TestDB
 logger.debug(f"sql_server_engine script uses url: {test_mssg}")
 
@@ -18,5 +19,12 @@ engine_SQLite_TestDB = create_engine(
     echo=True,  # https://docs.sqlalchemy.org/en/14/core/engines.html#sqlalchemy.create_engine.params.echo
     echo_pool=True,  # https://docs.sqlalchemy.org/en/14/core/engines.html#sqlalchemy.create_engine.params.echo_pool
     logging_name="SQLite test engine",
-    future=True)
+    future=False)
+
+url_hostname = build_full_url(
+    url=url_SQLite_TestDB,
+    odbc_driver="ODBC Driver for SQLite"
+)
+
+engine_sqlitetest_main = make_engine(url_hostname, timeout=100, verbose=True)
 
